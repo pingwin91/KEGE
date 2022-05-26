0adf9cf1ebd5bc521269aeae5079f2bb0055d2cc
igor <pingwin91@gmail.com>
2022-05-26 10:43:09 +0300

    Дополнил решение 24 задачи простым перебором строки

--- to260522/24.py
+++ to260522/24.py
@@ -1,7 +1,28 @@
-string = open('24.txt').readline().split('D')
-
+# через split()
+'''string = open('24.txt').readline().split('D')
+print(string)
 max_len = 0
 for i in range(len(string) - 1):
     max_len = max(max_len, len(string[i]) + len(string[i + 1]) + 1)
 
+print(max_len)'''
+
+
+#через перебор строки (долго, т.к. перебирает )
+f = open('24.txt')
+string = f.readline()
+max_len = 0
+
+for i in range(len(string)):
+    count_D = 0
+    len_D = 0
+    while count_D < 2 and i < len(string):
+        if string[i] != 'D':
+            len_D += 1
+            i += 1
+            max_len = max(max_len, len_D)
+        else:
+            count_D += 1
+            len_D += 1
+            i += 1
 print(max_len)
