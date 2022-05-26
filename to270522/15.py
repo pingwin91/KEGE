7d8877137978388657ba35193336519b96c1ec8c
igor <pingwin91@gmail.com>
2022-05-27 01:57:22 +0300

    корректировка 15 задания. Ответ не сходится (56 vs 57)

--- to270522/15.py
+++ to270522/15.py
@@ -2,14 +2,14 @@ def f(x, a1, a2):
     return (54 <= x <= 150) <= (((not (12 <= x <= 93)) and (not (a1 <= x <= a2))) <= (not(54 <= x <= 150)))
 
 z = []
-for a1 in range(150):
-    for a2 in range(150):
-        for x in range(150):
+for a1 in range(151):
+    for a2 in range(151):
+        for x in range(200):
             if f(x, a1, a2) == 0:
                 break
         else:
-            z.append(a2 - a1)
+            z.append((a2 - a1, a2, a1))
 
-print(min(z))
+print(sorted(z)[0])
 
-#  55
+#  56 (57???)
