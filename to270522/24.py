8aa7472da8592026d9131faf686c384814e53a5a
igor <pingwin91@gmail.com>
2022-05-27 10:39:25 +0300

    исправлено задание 12 + дополнено 24 не через split()

--- to270522/24.py
+++ to270522/24.py
@@ -1,6 +1,6 @@
-with open('24.txt') as f:
+'''with open('24.txt') as f:
     string = f.readline().split('AD')
-
+print(string)
 count = {'A': 0,
          'B': 0,
          'C': 0,
@@ -12,7 +12,31 @@ for i in range(len(string) - 1):
     else:
         count[string[i][-1]] += 1
 
-print(max(count) + str(count[max(count)]))
+print(max(count) + str(count[max(count)]))'''
 
 
 #  D1582
+
+
+with open('24.txt') as f:
+    string = f.readline()
+
+count_A = 0
+count_B = 0
+count_C = 0
+count_D = 0
+for i in range(1, len(string) - 1):
+    if string[i] == 'A' and string[i + 1] == 'D':
+        if string[i - 1] == 'A':
+            count_A += 1
+        if string[i - 1] == 'B':
+            count_B += 1
+        if string[i - 1] == 'C':
+            count_C += 1
+        if string[i - 1] == 'D':
+            count_D += 1
+
+print(f'A: {count_A}')
+print(f'B: {count_B}')
+print(f'C: {count_C}')
+print(f'D: {count_D}')
